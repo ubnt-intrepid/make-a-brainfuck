e703af3cc98f508c759310177affbda5c370033b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-01-04 23:06:47 +0900

    wip

--- ocaml/src/brainfuck.ml
+++ ocaml/src/brainfuck.ml
@@ -79,3 +79,53 @@ let rec _build_ast result tokens =
     _build_ast (List.append result [command]) (tail |> List.drop offset)
 
 let build_ast tokens = _build_ast [] tokens
+
+let parse source = source |> tokenize |> build_ast
+
+
+class tape = object (self)
+  val mutable the_pointer = (0 : int)
+  val mutable the_buffer = (Array.make 8096 (Char.chr 0) : char array)
+
+  method add_ptr n =
+    the_pointer <- the_pointer + n
+
+  method add_val n =
+    the_buffer.(the_pointer) <- Char.chr ((Char.code the_buffer.(the_pointer)) + n)
+
+  method get_char =
+    the_buffer.(the_pointer)
+
+  method put_char c =
+    the_buffer.(the_pointer) <- c
+
+  method core_dump =
+    ()
+end
+
+class interpreter = object (self)
+  val mutable the_tape = new tape
+
+  method eval source =
+    parse source
+    |> self#eval_lines
+
+  method eval_lines tokens =
+    tokens |> List.iter self#eval_token
+
+  method eval_token token =
+    match token with
+    | AddPtr n -> the_tape#add_ptr n
+    | AddVal n -> the_tape#add_val n
+    | PutChar ->
+      self#put_char the_tape#get_char
+    | GetChar -> ()
+    | CoreDump -> ()
+    | Loop ast ->
+      while the_tape#get_char != (Char.chr 0) do
+        self#eval_lines ast
+      done
+
+  method put_char c =
+    Printf.printf "%c" c
+end
--- ocaml/src/main.ml
+++ ocaml/src/main.ml
@@ -1,11 +1,4 @@
 let () =
-  let tokens = Brainfuck.tokenize "..++[>>+<<[<]\n]"
-  in
-  tokens
-  |> List.map (fun token -> Printf.printf "%s\n" (Brainfuck.show_token token))
-  |> ignore;
-  let ast = Brainfuck.build_ast tokens
-  in
-  ast
-  |> List.map (fun ast -> Printf.printf "%s\n" (Brainfuck.show_ast ast))
-  |> ignore
+  let i = new Brainfuck.interpreter in
+  i#eval "++>+++++[<+>-]++++++++[<++++++>-]<."
+
